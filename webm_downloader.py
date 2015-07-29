3cdd335005c696b376949c8c23c7297e11cc9012
Vlad Grech <inmosh@yandex.ru>
2015-07-29 19:10:29 +0300

    Started webm downloader

---
+++ webm_downloader.py
@@ -0,0 +1,92 @@
+# -*- coding: utf8 -*-
+
+import urllib.request
+import re
+import json
+import pprint
+import pickle
+import multiprocessing
+
+
+URL = 'https://2ch.hk/{}/catalog.json'
+QUERY = r'(webm | цуиь)'
+
+
+class DotDictionary(dict):
+
+    def __init__(self, init_value):
+        if not isinstance(init_value, dict):
+            raise TypeError('Expected dict, got {}'.format(type(init_value)))
+
+        self._dict_to_self(init_value)
+        super(DotDictionary, self).__init__()
+
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, val):
+        if isinstance(val, dict):
+            val = DotDictionary(val)
+        self[name] = val
+
+    def __setitem__(self, name, val):
+        if isinstance(val, dict):
+            val = DotDictionary(val)
+
+        super(DotDictionary, self).__setitem__(name, val)
+
+    def __delattr__(self, name):
+        del self[name]
+
+    def _dict_to_self(self, dct):
+        for key, value in dct.items():
+            if isinstance(value, (tuple, list)):
+                for i, _ in enumerate(value):
+                    if isinstance(value[i], dict):
+                        value[i] = DotDictionary(value[i])
+            self[key] = value
+
+
+class Catalog(object):
+
+    def __init__(self, board='b', local=False, get_data_on_init=True):
+        self.board = board
+        self.data = None
+
+        if local:
+            self.get_catalog = self._unpickle
+        else:
+            self.get_catalog = self._download
+
+        if get_data_on_init:
+            self.get_catalog()
+
+    def _get_filename(self):
+        return 'catalog_{}.pickle'.format(self.board)
+
+    def _unpickle(self):
+        try:
+            with open(self._get_filename(), 'rb') as f:
+                self.data = DotDictionary(pickle.load(f))
+        except (FileNotFoundError, pickle.UnpicklingError):
+            return
+
+    def _download(self):
+        raw_data = urllib.request.urlopen(URL.format(self.board))
+        self.json = raw_data.read().decode()
+        self.data = DotDictionary(json.loads(self.json))
+
+    def pickle(self):
+        with open(self._get_filename(), 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def search_threads(self, query=QUERY):
+        if not self.data:
+            return None
+
+        query = re.compile(query, flags=re.IGNORECASE | re.UNICODE)
+        for thread in self.data.threads:
+            if query.search(thread.comment):
+                print(thread.comment.decode('utf8'))
+                # print(thread.keys())
+                # print(thread.num)
